02755da3d17ec9c7dbf3093211596d135b776378
Mr-DeBonis <ignacio.debonis7@gmail.com>
2025-07-06 01:12:10 -0400

    Add Database form to add venues

--- events/views.py
+++ events/views.py
@@ -2,8 +2,10 @@ import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from events.forms import VenueForm
 from events.models import Event
 
 
@@ -37,3 +39,23 @@ def home(request, year=datetime.now().year, month=datetime.now().strftime('%B'))
 def all_events(request):
     event_list = Event.objects.all()
     return render(request, 'events/event_list.html', {'event_list': event_list})
+
+
+def add_venue(request):
+    submitted = False
+
+    if request.method == "POST":
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_venue?submitted=True')
+    else:
+        form = VenueForm
+        if 'submitted' in request.GET:
+            submitted = True
+
+    context = {
+        'form': form,
+        'submitted': submitted,
+    }
+    return render(request, 'events/add_venue.html', context=context)
